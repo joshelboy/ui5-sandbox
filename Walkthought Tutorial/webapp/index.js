788f6342a4ac53575bf4331949ffa88ba78edfce
Elias Tilegant <elias_tilegant@gmx.de>
2024-04-12 10:09:06 +0200

    Step 3

--- Walkthought Tutorial/webapp/index.js
+++ Walkthought Tutorial/webapp/index.js
@@ -1,5 +1,10 @@
 
-sap.ui.define([], () => {
+sap.ui.define([
+	"sap/m/Text"
+], (Text) => {
 	"use strict";
-	alert("UI5 is ready");
+
+	new Text({
+		text: "Hello World"
+	}).placeAt("content");
 });
